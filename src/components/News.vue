b3b9911061fc793243e9a7fcc0d032089f586308
Dianliang <dianliang233@gmail.com>
2020-10-01 09:13:49 +0800

    update home

--- src/components/News.vue
+++ src/components/News.vue
@@ -4,27 +4,42 @@
     <v-slide-group show-arrows>
       <v-slide-item>
         <v-card class="ma-2">
-            <v-card-text>placeholder</v-card-text>
+            <v-card-text>
+              <p>2020 年 10 月 1 日</p>
+              <p>茶馆工作室网站换新。</p>
+            </v-card-text>
           </v-card>
       </v-slide-item>
       <v-slide-item>
         <v-card class="ma-2">
-            <v-card-text>placeholder</v-card-text>
+            <v-card-text>
+              <p>2020 年 9 月 25 日</p>
+              <p><a href="https://dl.meme.teahou.se/">梗体中文 · 在线构建</a>大更新。</p>
+            </v-card-text>
           </v-card>
       </v-slide-item>
       <v-slide-item>
         <v-card class="ma-2">
-            <v-card-text>placeholder</v-card-text>
+            <v-card-text>
+              <p>2020 年 9 月 18 日</p>
+              <p><a href="https://ipe.js.org/">InPageEdit</a> 版梗体中文项目正式在 <a href="https://github.com/Teahouse-Studios/mcwzh-meme-ipe">GitHub</a> 启动。</p>
+            </v-card-text>
           </v-card>
       </v-slide-item>
       <v-slide-item>
         <v-card class="ma-2">
-            <v-card-text>placeholder</v-card-text>
+            <v-card-text>
+              <p>2020 年 9 月 17 日</p>
+              <p>梗体中文基岩版 <a href="https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack-bedrock/releases/tag/v0.7.0">v0.7.0</a> 发布，包含大量小型更新，并正在努力追赶 Java 版梗中进度，以发布正式版。</p>
+            </v-card-text>
           </v-card>
       </v-slide-item>
       <v-slide-item>
         <v-card class="ma-2">
-            <v-card-text>placeholder</v-card-text>
+            <v-card-text>
+              <p>2020 年 9 月 12 日、13 日</p>
+              <p>梗体中文 Java 版 <a href="https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack/releases/tag/v1.0.2">v1.0.2</a>、<a href="https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack/releases/tag/v1.0.3">v1.0.3</a> 发布，包含大量更新和修复。</p>
+            </v-card-text>
           </v-card>
       </v-slide-item>
     </v-slide-group>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,12 +56,140 @@
               </v-list-item>
 
               <v-card-actions>
-                <v-btn icon href="https://github.com/Teahouse-Studios/bo"><v-icon>mdi-github</v-icon></v-btn>
+                <v-btn icon href="https://github.com/Teahouse-Studios/bot"><v-icon>mdi-github</v-icon></v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
           <v-col sm="12" md="4">
             <!--placeholder-->
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+    <br><br>
+    <section>
+      <h2 class="text-center">成员</h2>
+      <v-container>
+        <v-row>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">Lxazl5770</v-list-item-title>
+                  <v-list-item-subtitle>中文 Minecraft Wiki 管理员、梗体中文资源包创始人、茶馆工作室创始人。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/lx.png" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/Lxazl5770"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">Dianliang233</v-list-item-title>
+                  <v-list-item-subtitle>中文 Minecraft Wiki 巡查员、茶馆工作室创始人。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/dl.png" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/Dianliang233"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">LakeJason</v-list-item-title>
+                  <v-list-item-subtitle>中文 Minecraft Wiki 巡查员、茶馆工作室创始人。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/lake.png" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/lakejason0"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">ff98sha</v-list-item-title>
+                  <v-list-item-subtitle>中文 Minecraft Wiki 行政员、茶馆工作室荣誉成员。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/ff.png" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/ff98sha"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">MysticNebula70</v-list-item-title>
+                  <v-list-item-subtitle>中文 Minecraft Wiki 管理员、茶馆工作室核心成员。曾为梗体中文资源包的构建系统作出重大贡献。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/my.jpg" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/MysticNebula70"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">_LittleC_</v-list-item-title>
+                  <v-list-item-subtitle>知名 Minecraft 服务器 <a href="https://www.yinwurealm.org/">YinwuRealm</a> 管理员、茶馆工作室核心成员、<del>御用全栈工程师</del>。曾为梗体中文资源包的构建系统作出重大贡献。</v-list-item-subtitle>
+                </v-list-item-content>
+
+                <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+                ><v-img src="@/assets/members/lcnb.png" /></v-list-item-avatar>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-btn icon href="https://github.com/_LittleC_"><v-icon>mdi-github</v-icon></v-btn>
+              </v-card-actions>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
