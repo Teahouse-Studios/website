cc5fa818a5e7a8e75754d88e40985cdb765c1797
Dianliang233 <dianliang233@gmail.com>
2022-05-03 15:05:42 +0800

    More footer improvements

--- src/components/TFooter.vue
+++ src/components/TFooter.vue
@@ -6,58 +6,100 @@
   >
     <v-container>
       <v-row>
-        <v-col md="6" sm="12" class="text-center text-md-left">
-          <span class="text-overline mr-3" style="vertical-align: middle"
-            >关注我们</span
-          >
-          <v-btn variant="text" icon href="https://teahouse.team/">
-            <v-icon size="small">{{ mdiWeb }}</v-icon>
-          </v-btn>
-          <v-btn variant="text" icon href="mailto:admin@teahou.se">
-            <v-icon size="small">{{ mdiEmail }}</v-icon>
-          </v-btn>
-          <v-btn
-            variant="text"
-            icon
-            href="https://space.bilibili.com/406275313"
-          >
-            <v-icon size="small">{{ siBilibili.path }}</v-icon>
-          </v-btn>
-          <v-btn variant="text" icon href="https://github.com/Teahouse-Studios">
-            <v-icon>{{ mdiGithub }}</v-icon>
-          </v-btn>
-          <v-btn variant="text" icon href="https://afdian.net/@teahouse">
-            <v-icon>{{ mdiCash }}</v-icon>
-          </v-btn>
-
-          <br />
-
-          <span class="text-overline mr-3" style="vertical-align: middle"
-            >法律</span
-          >
-          <v-btn variant="text" to="/terms/" :prepend-icon="mdiScript"
-            >服务条款</v-btn
-          ><v-btn variant="text" to="/privacy/" :prepend-icon="mdiLock"
-            >隐私政策</v-btn
-          >
+        <v-col
+          md="6"
+          sm="12"
+          class="text-center text-md-left"
+          style="line-height: 24px"
+        >
+          <div>
+            <span class="text-overline mr-3">关注我们</span>
+            <v-btn
+              variant="text"
+              icon
+              size="small"
+              href="https://teahouse.team/"
+            >
+              <v-icon size="small">{{ mdiWeb }}</v-icon>
+            </v-btn>
+            <v-btn
+              variant="text"
+              icon
+              size="small"
+              href="mailto:admin@teahou.se"
+            >
+              <v-icon size="small">{{ mdiEmail }}</v-icon>
+            </v-btn>
+            <v-btn
+              variant="text"
+              icon
+              size="small"
+              href="https://space.bilibili.com/406275313"
+            >
+              <v-icon size="small">{{ siBilibili.path }}</v-icon>
+            </v-btn>
+            <v-btn
+              variant="text"
+              icon
+              size="small"
+              href="https://github.com/Teahouse-Studios"
+            >
+              <v-icon>{{ mdiGithub }}</v-icon>
+            </v-btn>
+            <v-btn
+              variant="text"
+              icon
+              size="small"
+              href="https://afdian.net/@teahouse"
+            >
+              <v-icon>{{ mdiCash }}</v-icon>
+            </v-btn>
+          </div>
+          <div class="mb-1">
+            <span class="text-overline mr-3">产品</span>
+            <v-btn variant="text" href="https://meme.teahouse.team/">
+              梗体中文
+            </v-btn>
+            <v-btn
+              variant="text"
+              href="https://github.com/Teahouse-Studios/bot"
+            >
+              小可
+            </v-btn>
+          </div>
+          <div class="mb-1">
+            <span class="text-overline mr-3">法律</span>
+            <v-btn variant="text" to="/terms/" :prepend-icon="mdiScript"
+              >服务条款</v-btn
+            ><v-btn variant="text" to="/privacy/" :prepend-icon="mdiLock"
+              >隐私政策</v-btn
+            >
+          </div>
         </v-col>
         <v-col md="6" sm="12" class="text-center text-md-right pt-5">
-          <router-link to="/" title="首页">
-            <img
-              src="@/assets/logo.svg"
-              alt="Teahouse Studios"
-              height="48"
-              class="mx-auto ml-md-auto mr-md-0"
-              style="filter: invert(1)"
-            />
-          </router-link>
-          <div class="text-right text-body-2">
-            <a
-              class="text-white text-decoration-none"
-              href="https://beian.miit.gov.cn/"
-            >
-              蜀 ICP 备 2022011374 号-1
-            </a>
+          <div>
+            <router-link to="/" title="首页">
+              <img
+                src="@/assets/logo.svg"
+                alt="Teahouse Studios"
+                height="48"
+                class="mx-auto ml-md-auto mr-md-0"
+                style="filter: invert(1)"
+              />
+            </router-link>
+            <div class="text-right text-body-2 copyright">
+              <p>
+                <a
+                  class="text-white text-decoration-none"
+                  href="https://beian.miit.gov.cn/"
+                >
+                  蜀 ICP 备 2022011374 号-1
+                </a>
+              </p>
+              <p>
+                &copy; {{ new Date().getFullYear() }} 成都问谛居科技有限公司
+              </p>
+            </div>
           </div>
         </v-col>
       </v-row>
@@ -75,3 +117,8 @@ import {
 } from '@mdi/js'
 import { siBilibili } from 'simple-icons/icons'
 </script>
+<style>
+.copyright p {
+  line-height: 24px;
+}
+</style>
